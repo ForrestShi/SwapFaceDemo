0a04913b09c2e12f538f2c9f91d1a9a55eda8d42
Forrest <apple.dev.sh@gmail.com>
2012-06-09 17:05:08 +0800

    Initial Commit

---
+++ SwapFaceDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SwapFaceDemo
+//
+//  Created by Shi Forrest on 12-6-9.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
